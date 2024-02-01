5f66a4e65b01cc76a55c2d1d6820aabd3ccbb906
Sergei Vasilenko <sergei.vasilenko@satva.dev>
2024-02-01 21:55:43 +0530

    correct text

--- components/index.js
+++ components/index.js
@@ -1,5 +1,4 @@
 import "./nav-bar/nav-bar.js";
-import "./years-old/years-old.js";
 import "./date-interval/date-interval.js";
 import "./theme-toggle/theme-toggle.js";
 import "./contacts-list/contacts-list.js";
